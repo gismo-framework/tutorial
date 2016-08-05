949e4a91e04d663e758d0433f5500692da3b0f50
matthes <matthes@leuffen.de>
2016-08-06 00:53:56 +0200

    Initial

---
+++ src/Tutorial/Context/Frontend/FrontendContext.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthes
+ * Date: 05.08.16
+ * Time: 23:33
+ */
---
+++ www/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: matthes
+ * Date: 05.08.16
+ * Time: 23:31
+ */
+
+require __DIR__ . "/../vendor/autoload.php";
+
+
+
+
